4ee59da8f4702fd577ed57cabddff72431d42a92
Maruf Siddiki Galib <145563201+MarufSiddikiGalib@users.noreply.github.com>
2024-08-08 13:30:00 +0600

    fixed a bug . the bug is When you delete a row from a table with an auto-incrementing primary key in MySQL, the auto-increment value does not reset to fill the gap left by the deleted row. Instead, the next inserted row will have a primary key value that continues from the last used value.   Now the bug is fixed

--- crud_app/delete.php
+++ crud_app/delete.php
@@ -3,6 +3,29 @@
 
 <?php 
 
+function reorderAndReset($con) {
+   
+   // Reorder IDs
+   $reorderQuery = "SET @count = 0; UPDATE students SET id = @count:= @count + 1;";
+   if (mysqli_multi_query($con, $reorderQuery)) {
+       // Wait for the queries to finish
+       while (mysqli_next_result($con)) {;}
+   } else {
+       die("Query Failed: " . mysqli_error($con));
+   }
+
+   // Reset auto-increment value
+   $resetQuery = "ALTER TABLE students AUTO_INCREMENT = 1;";
+   if (!mysqli_query($con, $resetQuery)) {
+       die("Query Failed: " . mysqli_error($con));
+   }
+}
+
+
+
+
+
+
     if (isset($_GET['id'])){       //extract the id from url
          $id_new = $_GET['id'];
 
@@ -17,6 +40,7 @@
      }
 
      else{
+        reorderAndReset($con); // Call the reorder and reset function after deletion
         header("Location:index.php?delete_msg=Succesfully_deleted_the_data");  //dont forget it 
   }
 ?>
